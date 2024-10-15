8577d7fb1b2b459958c24488a41bd49ffd90fdc9
lahee12 <lahee1216@naver.com>
2024-10-15 16:21:11 +0900

    update

--- Test1/src/Hello.java
+++ Test1/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         // 이 클래스는 Hello World 출력하는 클래스입니다.
-         System.out.println("Hello world");
+         System.out.println("Hello world!!");
          System.out.println(1 + 2);
          System.out.println("1" + "2");
          System.out.println(3);
